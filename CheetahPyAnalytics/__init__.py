04882ab742430b32039c8ba3ede0bffcac273d39
RyanAugust <rad19206@yahoo.com>
2023-05-30 07:58:32 -0700

    version bump v0.1.0 -> v0.2.0

--- CheetahPyAnalytics/__init__.py
+++ CheetahPyAnalytics/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'CheetahPyAnalytics'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'RyanAugust'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2023'
@@ -20,6 +20,9 @@ from .CheetahPyAnalytics import (
     ,dataset_preprocess
     ,load_functions
     ,performance_functions)
+from .opendata import (
+    open_dataset
+)
 
 __all__ = [
     'CheetahPyAnalytics',
