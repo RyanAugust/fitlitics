0a03ccd05ec5ef3419f0b1fcdd1b44f7180cbcca
RyanAugust <rad19206@yahoo.com>
2023-06-17 08:35:18 -0400

    tests

---
+++ tests/tests.py
@@ -0,0 +1,35 @@
+from CheetahPyAnalytics import (
+    fetch_new_dataset,
+    dataset_preprocess,
+    metric_functions
+)
+import numpy as np
+import pandas as pd
+
+
+def metric_functions_activity_test():
+    frame = pd.DataFrame({'power':[0,1,2,3,4,5]})
+    zone_cutoffs = [0,2,4]
+    mets = metric_functions()
+    values = mets.activity_metric(
+        frame=frame, 
+        metric_name='TIZ', 
+        zone_cutoffs=zone_cutoffs)
+    assert values.sum() == 6
+
+def metric_functions_activity_summary_test():
+    frame = pd.DataFrame({'NP':[250]})
+    FTP = 300
+    mets = metric_functions()
+    value = mets.activity_summary_metric(
+        frame=frame,
+        metric_name='IF',
+        FTP=FTP)
+    assert value.values == [250.0/300.0]
+
+
+if __name__ == '__main__':
+    metric_functions_activity_test()
+    metric_functions_activity_summary_test()
+
+    print("Everything passed")
