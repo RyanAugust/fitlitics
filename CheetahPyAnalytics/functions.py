e9187832a4ce00e9c03dd3d48a6b5ae7918a5fb7
RyanAugust <rad19206@yahoo.com>
2023-06-19 19:08:33 -0400

    complete migration to unified metrics class

--- CheetahPyAnalytics/functions.py
+++ CheetahPyAnalytics/functions.py
@@ -4,10 +4,16 @@ import numpy as np
 class metric_functions:
     def __init__(self):
         self.activity_metric_function_map = {
-            'TIZ':   self._a_tiz,
-            'IF':    self._a_intensity_factor_power,
-            'NP':    self._a_normalized_power,
-            'TSS':   self._a_coggan_tss
+            'TIZ':            self._a_tiz,
+            'IF':             self._a_intensity_factor_power,
+            'NP':             self._a_normalized_power,
+            'TSS':            self._a_coggan_tss,
+            'VO2':            self._a_calc_vo2,
+            'max_power_ef':   self._a_max_Xmin_power_ef,
+            'max_power':      self._a_max_Xmin_power,
+            'hr_at_power':    self._a_hr_at_power,
+            'ae_ef':          self._a_ae_ef
+
         }
         self.activity_summary_metric_function_map = {
             'IF':    self._s_intensity_factor_power,
@@ -56,7 +62,7 @@ class metric_functions:
         """Takes input of an activty with power data and FTP setting and 
         returns the Normalized Power value"""
         _30sr_p = frame['power'].rolling(window=30, min_periods=1)
-        value = ( (_30sr_p**4).mean()**(1/4) ).value
+        value = ((_30sr_p**4).mean()**(1/4)).value
         return value
 
     def _s_coggan_tss(self, frame:pd.DataFrame, FTP:int=None) -> pd.Series:
@@ -82,127 +88,104 @@ class metric_functions:
         _tss = self._s_coggan_tss(frame=activity_summary, FTP=FTP)
         return _tss
     
-    def _a_calc_vo2(self, frame:pd.DataFrame, sport:str, resting_hr:int, max_hr:int) -> float:
+    def _a_calc_vo2(
+        self,
+        frame:pd.DataFrame,
+        sport:str,
+        resting_hr:int,
+        max_hr:int,
+        athlete_mass:float) -> float:
         """Takes input of an activity with power and hr data and returns an
         estimated VO2max value"""
-        
-    def _s_calc_vo2(self, frame:pd.DataFrame, hr_bounds:list[int]=[]) -> float:
-        """Takes input of an activity summary"""
+        _hr = frame['heart_rate'].mean()
+        _pct_VO2 = (_hr - resting_hr)/(max_hr - resting_hr)
 
-        try:
-            if row['Sport'] == 'Bike':
-                percent_vo2 = (row['Average_Heart_Rate'] - self.athlete_statics["resting_hr"])/(self.athlete_statics["max_hr"] - self.athlete_statics["resting_hr"])
-                vo2_estimated = (((row['Average_Power']/75)*1000)/row['Athlete_Weight']) / percent_vo2
-            elif row['Sport'] == 'Run':
-                percent_vo2 = (row['Average_Heart_Rate'] - self.athlete_statics["resting_hr"])/(self.athlete_statics["max_hr"] - self.athlete_statics["resting_hr"])
-                vo2_estimated = (210/row['xPace']) / percent_vo2
+        if sport == 'Bike':
+            _eff = (frame['power'].mean()/75)*1000/athlete_mass
+        elif sport == 'Run':
+            _eff = 210/frame['pace'].mean()
+        value = _eff / _pct_VO2
+        return value
+
+    def _s_calc_vo2(
+        self,
+        frame:pd.DataFrame, 
+        resting_hr:int=None,
+        max_hr:int=None,
+        athlete_mass:float=None) -> float:
+        """Takes input of an activity summary with power, heart rate, and sport data 
+        and returns estimated VO2max values"""
+        param_data = {'resting_hr':resting_hr,
+            'max_hr':max_hr,
+            'athlete_weight':athlete_weight}
+        for metric_name, metric_field in param_data.items():
+            # there has to be a better way to do this.... RESEARCH
+            if metric_name not in frame.columns:
+                assert metric_field is not None, f"Requires {metric_name} input in dataframe or as parameter"
             else:
-                vo2_estimated =  0.0
-            return vo2_estimated
-        except:
-            return 0.0
+                param_data[metric_name] = frame[metric_name]
 
+        _pct_VO2 = ((frame['average_heart_rate'] - param_data['resting_hr']) /
+                    (param_data['max_hr'] - param_data['resting_hr']))
 
+        _eff = np.where(frame['sport']=='Bike',
+                    (frame['power'].mean()/75)*1000/param_data['athlete_mass'],
+                    210/frame['pace'].mean())
+        values = _eff / _pct_VO2
+        return values
 
+    def _a_max_Xmin_power_ef(
+        self,
+        frame:pd.DataFrame,
+        power_duration:int) -> float:
+        rolling_power = frame['power'].rolling(window=power_duration).mean()
+        max_power_idx = rolling_power.argmax()
+        max_power = rolling_power.max()
+        max_power_hr = frame['heart_rate'][max_power_idx-power_duration:max_power_idx].mean()
+        value = max_power / max_power_hr
+        return value
 
-
-mets = metric_functions()
-
-
-class performance_functions(object):
-    def __init__(self, athlete_statics):
-        # super().__init__()
-        self.athlete_statics=athlete_statics
-        self.metric_function_map = {
-            'VO2':              self.calc_vo2,
-            'Garmin VO2':       self.use_garmin_vo2,
-            'AE EF':            self.calc_ae_ef,
-            'Power Index':      self.use_power_index,
-            'Power Index EF':   self.use_power_index_ef,
-            'Mod AE Power':     self.modeled_aerobic_threshold_power
-        }
+    def _a_max_Xmin_power(
+        self,
+        frame:pd.DataFrame,
+        power_duration:int) -> float:
+        value = frame['power'].rolling(window=power_duration).mean().max()
+        return value
     
-    def derive_performance(self, frame, performance_metric: str) -> float:
-        performance_function = self.metric_function_map[performance_metric]
-        values = []
-        for index, row in frame.iterrows():
-            values.append(performance_function(row))
-        frame['performance_metric'] = values
-        return frame
+    def _a_hr_at_power(
+        self,
+        frame:pd.DataFrame,
+        find_power_level:int,
+        power_duration:int=300,
+        tol:float=5) -> float:
+        """Takes input of an activity, a desired power level, power duration,
+        and a tolerance. Power values are taken at a 10-second rolling average and
+        tested against the assigned parameters
+        Returns a value for the heart rate sustainded during"""
+        rolling_power = frame['power'].rolling(window=10).mean()
+        u_rolling_power = rolling_power > find_power_level - tol
+        l_rolling_power = rolling_power < find_power_level + tol
+        rp_win_tol = u_rolling_power + l_rolling_power
+        rp_win_tol_idx = (rp_win_tol.rolling(window=power_duration) > power_duration * 2).argmin()
+        # I guess I should take the argmin to get the first occurance of stable power
+        value = frame['heart_rate'][rp_win_tol_idx-power_duration:rp_win_tol_idx].mean()
+        return value
 
-    ## TODO: REBUILD TO VECTOR MATH
-    # def derive_performance(self, frame: pd.DataFrame, performance_metric: str) -> pd.DataFrame:
-    #     performance_function = self.metric_function_map[performance_metric]
-    #     frame[performance_metric] = performance_function(frame)
-    #     return frame
-
-    def calc_vo2(self, row: pd.Series) -> float:
-        try:
-            if row['Sport'] == 'Bike':
-                percent_vo2 = (row['Average_Heart_Rate'] - self.athlete_statics["resting_hr"])/(self.athlete_statics["max_hr"] - self.athlete_statics["resting_hr"])
-                vo2_estimated = (((row['Average_Power']/75)*1000)/row['Athlete_Weight']) / percent_vo2
-            elif row['Sport'] == 'Run':
-                percent_vo2 = (row['Average_Heart_Rate'] - self.athlete_statics["resting_hr"])/(self.athlete_statics["max_hr"] - self.athlete_statics["resting_hr"])
-                vo2_estimated = (210/row['xPace']) / percent_vo2
-            else:
-                vo2_estimated =  0.0
-            return vo2_estimated
-        except:
-            return 0.0
-    ## TODO: REBUILD TO VECTOR MATH
-    # def calc_vo2(self, frame):
-    #     percent_vo2 = (frame['Average_Heart_Rate'] - self.athlete_statics["resting_hr"])/(self.athlete_statics["max_hr"] - self.athlete_statics["resting_hr"])
-    #     bike_vo2_estimated = (((frame['Average_Power']/75)*1000)/frame['Athlete_Weight']) / percent_vo2
-    #     run_vo2_estimated = (210/frame['xPace']) / percent_vo2
-
-    #     vo2_dict = {'Bike':bike_vo2_estimated
-    #                ,'Run': run_vo2_estimated}
-    #     for i,sport in frame['Sport'].unique():
-    #         vo2_dict.update({sport:0}) if sport not in vo2_dict.keys() else 0
-
-    #     values = frame['Sport'].apply(lambda sport: vo2_dict[sport])
-    #     return values
-
-    def use_garmin_vo2(self, row: pd.Series) -> float:
-        vo2_estimated = 0.0
-        if (row['Workout_Code'] != 'Rec') & (row['Sport'] in ['Run','Bike']):
-            vo2_estimated = row['VO2max_detected'] # Garmin VO2 Estimation
-        return vo2_estimated
-
-    def calc_ae_ef(self, row: pd.Series) -> float:
-        ef = 0
-        if (row['Workout_Code'] == 'AE'):
-            if row['Sport'] == 'Bike':
-                ef = row['IsoPower']/row['Average_Heart_Rate']
-            elif row['Sport'] == 'Run':
-                ef = row['IsoPower']/row['Average_Heart_Rate']
+    def _a_ae_ef(
+            self,
+            frame:pd.DataFrame,) -> float:
+        ef = np.where(frame['sport'] in ['Bike','Run'],
+                      frame['IsoPower']/frame['Average_Heart_Rate'],
+                      np.nan)
         return ef
-    
-    def use_power_index(self, row: pd.Series) -> float:
-        if row['Average_Power'] > 0:
-            val = row['Power_Index']
-        else:
-            val = 0.0
-        return val
 
-    def use_power_index_ef(self, row):
-        if row['Average_Power'] > 0:
-            hr_range = self.athlete_statics['max_hr'] - self.athlete_statics['resting_hr']
-            avg_hr_rel = row['Average_Heart_Rate'] - self.athlete_statics['resting_hr']
-            relative_hr = (avg_hr_rel / hr_range)*100
-            
-            pi_ef = row['Power_Index']/relative_hr
-            val = pi_ef
-        else:
-            val = 0
-        return val
 
-    def modeled_aerobic_threshold_power(self, row):
-        temp = 20
-        duration = 60*60
+    # def modeled_aerobic_threshold_power(self, row):
+    #     temp = 20
+    #     duration = 60*60
         
-        if (row['a'] != 0) & (row['Duration'] > 999):
-            power = row['a'] + row['b'] * self.athlete_statics['threshold_hr'] +  row['c'] * duration * temp
-            return power
-        else:
-            return 0
+    #     if (row['a'] != 0) & (row['Duration'] > 999):
+    #         power = row['a'] + row['b'] * self.athlete_statics['threshold_hr'] +  row['c'] * duration * temp
+    #         return power
+    #     else:
+    #         return 0
