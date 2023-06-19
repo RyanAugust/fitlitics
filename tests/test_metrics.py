94260517c8e158692b632dfd05d902d8e49e2230
RyanAugust <rad19206@yahoo.com>
2023-06-19 19:09:40 -0400

    new tests for functions

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -10,22 +10,40 @@ import pandas as pd
 def test_metric_functions_activity():
     frame = pd.DataFrame({'power':[0,1,2,3,4,5]})
     zone_cutoffs = [0,2,4]
-    mets = metric_functions()
-    values = mets.activity_metric(
-        frame=frame, 
-        metric_name='TIZ', 
+    metric_funcs = metric_functions()
+    values = metric_funcs.activity_metric(
+        frame=frame,
+        metric_name='TIZ',
         zone_cutoffs=zone_cutoffs)
     assert values.sum() == 6
 
 def test_metric_functions_activity_summary():
     frame = pd.DataFrame({'NP':[250]})
     FTP = 300
-    mets = metric_functions()
-    value = mets.activity_summary_metric(
+    metric_funcs = metric_functions()
+    value = metric_funcs.activity_summary_metric(
         frame=frame,
         metric_name='IF',
         FTP=FTP)
     assert value.values == [250.0/300.0]
+
+def test_activity_VO2_calc():
+    frame = pd.DataFrame({'power':[200,200,200,200],
+                          'heart_rate':[110,110,110,110],
+                          'pace':[14,14,14,14]})
+    sport = 'Bike'
+    resting_hr = 40
+    max_hr = 190
+    athlete_mass = 85
+    metric_funcs = metric_functions()
+    value = metric_funcs.activity_metric(
+        frame=frame,
+        metric_name='VO2',
+        sport = sport,
+        resting_hr = resting_hr,
+        max_hr = max_hr,
+        athlete_mass = athlete_mass)
+    assert value == [((200/75*1000)/85)/((110-40)/(190-40))]
 
 
 # if __name__ == '__main__':
